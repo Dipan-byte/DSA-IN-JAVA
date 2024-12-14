02cd7fb8800d7e543202773544f9c798d41c89a9
Dipan-byte <dipanmallick39@gmail.com>
2024-12-14 18:34:07 +0530

    your wold

--- HALLOW_WORLD.java
+++ HALLOW_WORLD.java
@@ -1,8 +1,8 @@
-package BASIC;
+
 
 public class HALLOW_WORLD {
     public static void main(String[] args){
-       System.out.println("HALLOW WORLD");
+       System.out.println("your WORLD");
     }
 }
 
