77abe49553ed6c4e38646de333af6a1ec801cc57
DIPAN MALLICK <dipanmallick39@gmail.com>
2025-01-31 17:08:40 +0000

    not solved

---
+++ BASIC/problems/printing_table_of_number.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class printing_table_of_number{
+    public static void main(){
+        Scanner input=new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER : ");
+        
+    }
+}
