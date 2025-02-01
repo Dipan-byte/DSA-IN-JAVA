5a5d3de79f1e5a9be6b4f109b26c07fc7e407162
DIPAN MALLICK <dipanmallick39@gmail.com>
2025-02-01 14:01:55 +0000

    done

--- BASIC/problems/printing_table_of_number.java
+++ BASIC/problems/printing_table_of_number.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 public class printing_table_of_number{
-    public static void main(){
+    public static void Table(int limit , int number ){
+        for(int i=0 ; i<=limit ; i++){
+            int answer = number*i ;
+            System.out.println(number + "*" + i + "=" + answer);
+        }
+    }
+    public static void main( String[] args){
         Scanner input=new Scanner(System.in);
         System.out.print("ENTER THE NUMBER : ");
-        
+        int number= input.nextInt() ;
+        System.out.print("ENTER THE LIMIT : ");
+        int limit= input.nextInt() ;
+        Table(limit , number );
     }
 }
