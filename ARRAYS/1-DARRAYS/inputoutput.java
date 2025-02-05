3727aef1d747386c1909273d16c8db47dc7f4ec4
DIPAN MALLICK <dipanmallick39@gmail.com>
2025-02-05 10:11:25 +0000

    done

--- ARRAYS/1-DARRAYS/inputoutput.java
+++ ARRAYS/1-DARRAYS/inputoutput.java
@@ -1,24 +1,28 @@
 import java.util.Scanner ;
 public class inputoutput{
-    public static String[] arrayinput(int size){
+    public static String[] arrayinput(int size , Scanner sc){
         String inputarray[]= new String[size] ;
-        for(int i=0 ; i<n ; i++){
-             inputarray[]=sc.next();
+        for(int i=0 ; i<size ; i++){
+            System.out.println("ENTER THE ARRAY NUMBER AT THE INDEX : " + i);
+             inputarray[i]=sc.next();
         }
-        return inputarray[];
+        return inputarray ;
     }
-    public static void arraypaste(String array[] int size){
-        String pastearray[]= array[] ;
+    public static void arraypaste(String array[] , int size){
+        String pastearray[]= array ;
+        System.out.println("THE ARRAY IS : ");
         for(int i=0 ; i<size ; i++){
             System.out.print(pastearray[i] + " ");
         }
+            System.out.print("\n") ;
     }
     public static void main(String args[]){
-        Scanner sc= new Scanner(system.in);
+        Scanner sc= new Scanner(System.in);
+        System.out.print("ENTER THE ARRAY SIZE : ");
         int size= sc.nextInt();
-        String array[] = arrayinput(size);
-        arraypaste(array[] size );
-        
+        String array[] = arrayinput(size , sc);
+        arraypaste(array , size );
+        sc.close();
     }
     
 }
