d7f54e39f99a25de0d282b47ffc7d4b14569d265
Dipan Mallick <dipanmallick39@gmail.com>
2025-12-16 23:12:55 +0530

    done

---
+++ ARRAYS/1-DARRAYS/maxsubarr.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MaxSumSubarrayK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int k = sc.nextInt();
+
+        int windowSum = 0;
+        for (int i = 0; i < k; i++)
+            windowSum += arr[i];
+
+        int maxSum = windowSum;
+
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        System.out.println("Maximum Sum: " + maxSum);
+    }
+}
